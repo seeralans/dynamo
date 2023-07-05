c68d9587c69e10484e35cb1cef2be7cb342e4029
Seeralan Sarvaharman <s.sarvaharman@bristol.ac.uk>
2023-07-05 16:40:11 +0100

    remove uncessary setup.py file, using pyproject.toml instead

--- rust/dynamo-base/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='dynamo',
-      version='0.0.1',
-      packages=find_packages(include=["dynamo", 'dynamo.*']),
-      description='A package for dynamical modelling of protein structures',')
